71298fc10c0c083c6fdf3a199633495d1414a741
Gabriel Marcano <88866343+gabrielmarcano@users.noreply.github.com>
2022-07-13 22:46:14 -0400

    Working script

--- script.sh
+++ script.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-curl -s https://peapix.com/bing/feed?country=jp | jq -r '.[0].fullUrl'
+# Get the url from the API. Get the US wallpapers
+URL=$(curl -s https://peapix.com/bing/feed?country=us | jq -r '.[0].fullUrl')
+
+curl -s $URL > /tmp/image.jpg
+
+gsettings set org.gnome.desktop.background picture-uri file:///tmp/image.jpg
