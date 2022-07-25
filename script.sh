f9112f30a15103c68b05338f1ed19fa6626ffce0
Gabriel Marcano <88866343+gabrielmarcano@users.noreply.github.com>
2022-07-25 13:34:15 -0400

    Fix missing quotation mark

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Get the url from the Bing API. Get the AU wallpapers
-URL="https://www.bing.com$(curl -s https://www.bing.com/HPImageArchive.aspx?format=js\&idx=0\&n=1\&mkt=en-AU | jq -r '.images[0].url')
+URL="https://www.bing.com$(curl -s https://www.bing.com/HPImageArchive.aspx?format=js\&idx=0\&n=1\&mkt=en-AU | jq -r '.images[0].url')"
 
 curl -s $URL > $(pwd)/BingWallpaper.jpg
 
