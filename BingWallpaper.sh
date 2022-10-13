28354118f251b3c1a456e1bda822da3224ed76ba
Gabriel Marcano <88866343+gabrielmarcano@users.noreply.github.com>
2022-10-13 17:41:30 -0400

    One line change
    Save image and change background after the images downloads succesfully

--- BingWallpaper.sh
+++ BingWallpaper.sh
@@ -3,6 +3,8 @@
 # Get the URL from the Bing API. Get the AU wallpapers
 URL="https://www.bing.com$(curl -s https://www.bing.com/HPImageArchive.aspx?format=js\&idx=0\&n=1\&mkt=en-AU | jq -r '.images[0].url')"
 
-curl -s $URL > $(pwd)/BingWallpaper.jpg
+# Save the image path in a constant
+IMAGE="$(pwd)/BingWallpaper.jpg"
 
-gsettings set org.gnome.desktop.background picture-uri file://$(pwd)/BingWallpaper.jpg
+# Save the fetched image to a local image, and if that passes succesfully (&&), change the background wallpaper with that image
+curl -s $URL > $IMAGE && gsettings set org.gnome.desktop.background picture-uri file://$IMAGE
