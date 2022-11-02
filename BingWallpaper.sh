adacd1b5fb32afff77e5a293706bd5a2bf29a4b8
Gabriel Marcano <88866343+gabrielmarcano@users.noreply.github.com>
2022-11-01 23:14:23 -0400

    Add TODO about dirname $0

--- BingWallpaper.sh
+++ BingWallpaper.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+# TODO: Read about dirname $0 to fix relative current working directory (https://bytexd.com/what-is-dirname-0-and-usage-examples/)
+
 # Get the URL from the Bing API. Get the AU wallpapers
 URL="https://www.bing.com$(curl -s https://www.bing.com/HPImageArchive.aspx?format=js\&idx=0\&n=1\&mkt=en-AU | jq -r '.images[0].url')"
 
